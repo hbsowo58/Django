c64a76d080c010fcd7e4d96a971bcb2e33377bf0
hbs <hbsowo58@gmail.com>
2023-09-14 01:15:57 +0900

    update : add gitignore & delete venv

--- 001django_part1/venv/Scripts/activate.bat
+++ 001django_part1/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\user\Desktop\Django\000django_part1\venv
+set VIRTUAL_ENV=C:\Users\user\Desktop\Django\001django_part1\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
